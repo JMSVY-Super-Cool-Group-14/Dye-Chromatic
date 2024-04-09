8c497642a3421e88d1e060c5b5f1d79cb8966d6e
Julia Wang <julia.v.wang@gmail.com>
2024-04-09 17:53:38 +0200

    Add main scene and bus layout

---
+++ src/scripts/main.gd
@@ -0,0 +1,34 @@
+extends CanvasLayer
+
+@onready var buttons = $"Center/Buttons"
+@onready var options = $"Center/Options"
+@onready var credits = $"Center/Credits"
+@onready var bop = $Bop
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_mouse_entered():
+	bop.play(0.0)
+
+func _on_exit_game_pressed():
+	get_tree().quit()
+
+func _close_screens():
+	options.visible = false
+	credits.visible = false
+	visible = true
+
+func _on_credits_pressed():
+	credits.visible = true
+	visible = false
+
+func _on_options_pressed():
+	options.visible = true
+	visible = false
