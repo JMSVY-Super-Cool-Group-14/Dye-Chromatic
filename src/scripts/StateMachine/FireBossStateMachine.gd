d4704b92fd964f6d70ee959054826c5672c6f062
Vilhelm Norström <Vilhelmnorst@gmail.com>
2024-04-29 13:21:00 +0200

    Fire Boss Framework

---
+++ src/scripts/StateMachine/FireBossStateMachine.gd
@@ -0,0 +1,18 @@
+extends FiniteStateMachine
+
+
+@export var health = 100
+
+func take_damage(dmg):
+	health -= dmg
+
+func _ready():
+	get_node("Idle")._initialize_state(self, root)
+	super()
+	
+
+func _change_state(new_state: State):
+	super(new_state)
+
+func _process(delta):
+	super(delta)
