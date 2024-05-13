959281bec77a4765ae62191ac04ef31df6a7f4ad
Julia Wang <julia.v.wang@gmail.com>
2024-05-13 17:08:01 +0200

    UI fixes

--- src/scripts/ui/input_settings.gd
+++ src/scripts/ui/input_settings.gd
@@ -1,5 +1,8 @@
 extends VBoxContainer
 
+# Credit to DashNothing
+# https://www.youtube.com/watch?v=ZDPM45cHHlI
+
 @onready var input_button = preload("res://scenes/ui/input_button.tscn")
 @onready var actions = $"./ScrollContainer/Actions"
 
@@ -15,10 +18,6 @@ func _ready():
 	control_avail = Input.get_connected_joypads().size() > 0
 	Input.joy_connection_changed.connect(connection_changed)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func _create_actions():
 	InputMap.load_from_project_settings()
 	for item in actions.get_children():
@@ -32,11 +31,23 @@ func _create_actions():
 		action_label.text = action
 		var events = InputMap.action_get_events(action)
 		if events.size() > 0:
-			input_label.text = events[0].as_text()
+			input_label.text = events[0].as_text().trim_suffix(" (Physical)")
 		else:
 			input_label.text = ""
 			
 		actions.add_child(button)
+		button.pressed.connect(_on_button_pressed.bind(button, action))
+
+func _input(event):
+	if remapping:
+		pass
+
+func _on_button_pressed(button, action):
+	if !remapping:
+		remapping = true
+		action_to_remap = action
+		remapping_button = button
+		button.find_child("Input").text = "Press key..."
 
 func connection_changed(_device, connected):
 	control_avail = connected
