eb78b1ed21371a02213d86745b6e51304c54f6be
Julia Wang <julia.v.wang@gmail.com>
2024-05-20 14:58:24 +0200

    Add hpbar to enemy

--- src/scripts/StateMachine/EnemyStateMachine.gd
+++ src/scripts/StateMachine/EnemyStateMachine.gd
@@ -1,6 +1,7 @@
 extends FiniteStateMachine
 class_name enemyStateMachine
 
+@onready var hpbar = $"../HPbar"
 
 @export var health = 100
 @export var attack_range := 10.0
@@ -13,6 +14,11 @@ signal took_dmg
 
 func take_damage(dmg):
 	health -= dmg
+	if health < 100:
+		hpbar.visible = true
+	elif health == 0:
+		hpbar.visible = false
+	hpbar.value = health
 	took_dmg.emit()
 	
 func take_DOT_damage(dmg, duration):
@@ -24,9 +30,12 @@ func take_DOT_damage(dmg, duration):
 	
 
 func _ready():
-	get_node("Idle")._initialize_state(self, root)
 	super()
+	get_node("Idle")._initialize_state(self, root)
 	
+	hpbar.max_value = 100
+	hpbar.value = health
+	hpbar.visible = false
 
 func _change_state(new_state: State):
 	super(new_state)
